9c28b6693cee5f52bddc293b0eccaf720c63fda0
Caroline Ferreira <carolinefcgomes@gmail.com>
2023-05-25 20:25:02 -0300

    Update 44 - Subqueries.sql

--- 44 - Subqueries.sql
+++ 44 - Subqueries.sql
@@ -9,3 +9,84 @@ WHERE ID_Loja = (
                 FROM base.lojas
                 WHERE Gerente = 'Marcelo Castro');
 
+# Exemplo 2: produto com preço acima da média
+
+SELECT 
+    *
+FROM
+    base.produtos
+WHERE Preco_Unit > (
+					SELECT AVG(Preco_Unit) 
+					FROM base.produtos);
+                    
+# Exemplo 3 (Desafio): Descubra qual foi o cliente que mais gerou receita para a empresa
+
+use base;
+select * from clientes;
+select * from pedidos;
+
+
+select c.*
+from clientes as c
+INNER JOIN (
+select ID_Cliente, Receita
+from (	select ID_Cliente, sum(Receita_Venda) as 'Receita'
+		from pedidos
+		group by ID_Cliente
+	 ) as p
+where Receita = (SELECT MAX(Receita) 
+from (	select ID_Cliente, sum(Receita_Venda) as 'Receita'
+		from pedidos
+		group by ID_Cliente) as p
+	 )
+group by ID_Cliente) as pe
+ON c.ID_Cliente = pe.ID_Cliente;
+
+
+
+ -- Solução Hastag
+ 
+ SELECT * 
+ FROM clientes
+ WHERE ID_Cliente = (
+					SELECT ID_Cliente
+                    FROM pedidos
+                    GROUP BY ID_Cliente
+                    ORDER BY SUM(Receita_Venda) DESC
+                    LIMIT 1
+                    );
+ 
+ 
+ # Exemplo 3: receita total dos produtos da marca DELL
+ SET @marca_prod = 'DELL';
+ 
+SELECT SUM(Receita_Venda) as 'Receita total'
+ FROM
+    pedidos
+WHERE
+    ID_Produto IN (
+				 SELECT ID_Produto
+				 FROM produtos
+				 WHERE Marca_Produto = @marca_prod
+                 );
+                 
+# Exemplo 4: Quais pedidos foram feitos na região Sudeste
+
+
+SELECT * from pedidos;
+SELECT * from clientes;
+SELECT * FROM lojas;
+SELECT * from locais;
+
+SET @regiao = 'Sudeste';
+SELECT * 
+FROM PEDIDOS
+WHERE ID_Loja IN (
+				SELECT ID_Loja
+                FROM lojas
+                WHERE Loja IN (
+							SELECT Cidade
+							FROM locais
+							WHERE Região = @regiao
+                            )
+				);
