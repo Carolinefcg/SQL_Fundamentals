19fa323bc9e992d16bc852bfc3b63a364a2e00db
Caroline Ferreira <carolinefcgomes@gmail.com>
2023-06-19 10:35:29 -0300

    Update 21 - CRUD.sql

--- SQLServer/21 - CRUD.sql
+++ SQLServer/21 - CRUD.sql
@@ -55,3 +55,59 @@ select * from Produtos;
 
 DROP TABLE Produtos
 DROP DATABASE BDImpressionador
+
+
+--------------------------------------------------------------------
+
+USE BDImpressionador
+
+-- DROP TABLE Funcionarios
+
+CREATE TABLE Funcionarios(
+	id_funcionario int,
+	nome_funcionario varchar(100),
+	salario float,
+	data_nascimento DATETIME
+)
+
+INSERT INTO Funcionarios(id_funcionario, nome_funcionario, salario, data_nascimento)
+VALUES
+	(1, 'Lucas'		, 1500, '19900320'),
+	(2, 'Andressa'	, 2300, '19881207'),
+	(3, 'Felipe'	, 4000, '19930213'),
+	(4, 'Marcelo'	, 7100, '19930410'),
+	(5, 'Carla'		, 3200, '19860902'),
+	(6, 'Juliana'	, 5500, '19890121'),
+	(7, 'Mateus'	, 1900, '19931102'),
+	(8, 'Sandra'	, 3900, '19900509'),
+	(9, 'André'		, 1000, '19940313'),
+	(10, 'Julio'	, 4700, '19920705')
+
+SELECT * FROM Funcionarios
+
+-- ALTER TABLE
+
+-- ADD COLUMN
+GO
+ALTER TABLE Funcionarios
+ADD cargo VARCHAR(100), bonus FLOAT
+GO
+
+UPDATE Funcionarios
+SET CARGO = 'Analista', bonus = 0.15
+WHERE id_funcionario = 1
+
+SELECT * FROM Funcionarios
+
+
+-- ALTER DATA TYPE
+GO
+ALTER TABLE Funcionarios
+ALTER COLUMN salario INT
+GO
+-- DROP COLUMN
+ALTER TABLE Funcionarios
+DROP COLUMN	cargo, bonus
+
+SELECT * FROM Funcionarios
+
