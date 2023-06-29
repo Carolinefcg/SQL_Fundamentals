47488fd897b9d7e96f33548e4967a85d2f2d20ef
Caroline Ferreira <carolinefcgomes@gmail.com>
2023-06-29 10:00:15 -0300

    Update 29 - TRANSACTIONS.sql

--- SQLServer/29 - TRANSACTIONS.sql
+++ SQLServer/29 - TRANSACTIONS.sql
@@ -29,38 +29,57 @@ ROLLBACK TRANSACTION
 SELECT * FROM clientes_aux
 
 -- EXAMPLE 2: DELETE TRANSACTION AND COMMIT TRANSACTION AND (attempt to) ROLLBACK
-BEGIN TRANSACTION
+BEGIN TRANSACTION T1
 DELETE 
 FROM clientes_aux
 WHERE id_cliente = 1
 
 SELECT * FROM clientes_aux
 
-COMMIT TRANSACTION
+COMMIT TRANSACTION T1
 
 SELECT * FROM clientes_aux
 
 -- will return an error message
-ROLLBACK TRANSACTION
+ROLLBACK TRANSACTION T1
 
 SELECT * FROM clientes_aux
 
 
 -- EXAMPLE 3: UPDATE TRANSACTION AND ROLLBACK AND (attempt to) COMMIT TRANSACTION
-BEGIN TRANSACTION
+BEGIN TRAN T2
 UPDATE clientes_aux
 SET CPF = '000.000.000-01'
 WHERE id_cliente = 2
 
 SELECT * FROM clientes_aux
 
-ROLLBACK TRANSACTION
+ROLLBACK TRAN T2
 
 SELECT * FROM clientes_aux
 
 -- will return an error message
-COMMIT TRANSACTION
+COMMIT TRAN T2
 
 SELECT * FROM clientes_aux
 
+/*CONDITIONAL INSERT*/
 
+DECLARE @cpf_insert VARCHAR(20) = '193.174.192-82'
+BEGIN TRANSACTION T3
+INSERT INTO clientes_aux(Nome_Cliente, Genero, Data_de_Nascimento,  CPF)
+	VALUES('Testing Guy', 'M', '19990909', @cpf_insert)
+
+DECLARE @cpf_check VARCHAR(20) = (SELECT count(*) from clientes_aux WHERE CPF = @cpf_insert)
+SELECT @cpf_check
+
+IF @cpf_check> 1 -- only save unique cpfs
+	BEGIN
+	ROLLBACK TRAN T3
+	END
+ELSE 
+	BEGIN
+	COMMIT TRAN T3
+	END
+
+SELECT * FROM clientes_aux
